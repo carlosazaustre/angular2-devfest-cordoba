3b882107600429006f7ca32911d88de160c44d68
Carlos Azaustre <cazaustre@gmail.com>
2015-11-05 13:26:47 +0100

    add hello and hello-app component

--- src/app.js
+++ src/app.js
@@ -1,17 +1,49 @@
-import {Component, View, bootstrap} from 'angular2/angular2';
+import 'zone.js/lib/browser/zone-microtask';
+import 'reflect-metadata';
+import 'babel-core/polyfill';
 
+import {
+  Component, View, Attribute,
+  provide, bootstrap
+} from 'angular2/angular2';
+import {
+  Router, RouteConfig, RouteParams,
+  LocationStrategy, HashLocationStrategy,
+  ROUTER_PROVIDERS, ROUTER_DIRECTIVES, ROUTER_PRIMARY_COMPONENT
+} from 'angular2/router';
+
+// -- <hello> Component --------------------------------------------------------
 @Component({
-  selector: 'hello-app'
+  selector: 'hello'
 })
-
 @View({
   template: '<p>{{ message }}</p>'
 })
-
-class HelloApp {
+class Hello {
   constructor() {
-    this.message = "Hola DevFest!";
+    this.message = "Hola GDG #DevFestCordoba!";
   }
 }
 
-bootstrap(HelloApp, []);
+// -- <hello-app> Component ----------------------------------------------------
+@Component({
+  selector: 'hello-app'
+})
+@View({
+  directives: [ROUTER_DIRECTIVES],
+  template: `<router-outlet></router-outlet>`
+})
+@RouteConfig([
+  { path: '/', component: Hello, as: 'Hello' }
+])
+class HelloApp {
+  
+}
+
+
+// -- Bootstrap App ------------------------------------------------------------
+bootstrap(HelloApp, [
+  ROUTER_PROVIDERS,
+  provide(LocationStrategy, { useClass: HashLocationStrategy }),
+  provide(ROUTER_PRIMARY_COMPONENT, { useValue: HelloApp })
+]);
